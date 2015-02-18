a35434c78b88342333273f2211a628735016ec9c
Mihaela Encheva <dyct.ent@gmail.com>
2015-02-18 21:56:10 +0200

    now works with singles

--- Homeworks/C#2/Methods/ReverseNumber/ReverseNumber.cs
+++ Homeworks/C#2/Methods/ReverseNumber/ReverseNumber.cs
@@ -3,26 +3,41 @@
 namespace ReverseNumber
 {
     using System;
+    using System.Text;
+
     class ReverseNumber
     {
         static void Main(string[] args)
         {
             Console.Write("Enter a number: ");
-            int number = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
 
-            Console.WriteLine(Reverse(number));
+            Console.WriteLine(Reverse(input));
         }
 
-        private static int Reverse(int number)
+        private static double Reverse(string number)
         {
-            int result = 0;
-            while (number > 0)
+            char[] num = number.ToCharArray();
+            char[] result = new char[num.Length];
+
+            for (int index = 0; index < result.Length; index++) 
+                if (!Char.IsDigit(num[index])) 
+                    result[index] = num[index];
+
+            for (int resultInd = result.Length - 1, numInd = 0;
+                 resultInd >= 0 && numInd < num.Length;
+                 resultInd--, numInd++) 
             {
-                result *= 10;
-                result += number % 10;
-                number /= 10;
+                while (result[resultInd]!='\0')
+                    --resultInd;
+
+                while (!Char.IsDigit(num[numInd])) 
+                    ++numInd;
+
+                result[resultInd] = num[numInd];
             }
-            return result;
+
+            return double.Parse(new String(result));
         }
     }
 }
