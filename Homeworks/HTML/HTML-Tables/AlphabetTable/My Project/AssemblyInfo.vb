8bf05b274f981d03bd008ceb4b0a005d0d068cd2
dentia <dyct.ent@gmail.com>
2015-04-10 12:00:10 +0300

    [HW][HTML] HTML homework
    Telerik Academy 2015/2016
    HTML homewrok

---
+++ Homeworks/HTML/HTML-Tables/AlphabetTable/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("AlphabetTable")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("AlphabetTable")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8e632c7a-243a-4939-9c94-0a1f93ad9bf0")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Homeworks/HTML/HTML-Tables/Calculator/logic.js
@@ -0,0 +1,17 @@
+﻿function addToDisplay(value) {
+
+    if (value === "BS") {
+        var length = display.value.length - 1;
+        value = display.value.substring(0, length);
+        display.value = "";
+    }
+    else if (value === "C") {
+        display.value = value = "";
+    }
+    else if (value === "=") {
+        value = eval(display.value);
+        display.value = "";
+    }
+
+    display.value += value;
+}
