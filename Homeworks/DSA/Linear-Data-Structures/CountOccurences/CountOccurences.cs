64a6c4c243a82bf7bc5b1eecc9b2d99f457ce5ca
dentia <dyct.ent@gmail.com>
2015-05-02 15:13:35 +0300

    [HW][DSA][TA2014] Linear-Data-Structures
    Telerik Academy 2015/2016
    Linear Data Structures

---
+++ Homeworks/DSA/Linear-Data-Structures/CountOccurences/CountOccurences.cs
@@ -0,0 +1,39 @@
+﻿// Write a program that finds in given array of integers
+// (all belonging to the range [0..1000]) how many
+// times each of them occurs.
+namespace CountOccurences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Common;
+
+    class CountOccurences
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter numbers on separate lines. The sequence should end with an empty line.");
+            List<int> numbers = Common.ReadInput();
+
+            var groups = numbers.GroupBy(number => number).Select(group => group.ToArray()).OrderBy(group => group[0]);
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine("{0} -> {1}", group[0], group.Count());
+            }
+        }
+    }
+}
+
+/*
+3
+4
+4
+2
+3
+3
+4
+3
+2
+
+ */
---
+++ Homeworks/DSA/Linear-Data-Structures/Queue/QueueTest.cs
@@ -0,0 +1,26 @@
+﻿// Implement the ADT queue as dynamic linked list.
+// Use generics (LinkedQueue<T>) to allow storing
+// different data types in the queue.
+
+namespace Queue
+{
+    class QueueTest
+    {
+        static void Main()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            System.Console.WriteLine("Count: " + queue.Count);
+
+            while (queue.Count > 0)
+            {
+                System.Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}
