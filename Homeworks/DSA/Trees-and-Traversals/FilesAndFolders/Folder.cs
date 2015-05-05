9d490b5974d8da097b3e1b7187742d104bd79c7c
dentia <dyct.ent@gmail.com>
2015-05-05 17:02:40 +0300

    [HW][DSA][TA2014] Trees-and-Traversals
    Telerik Academy 2015/2016
    Trees and Traversals

---
+++ Homeworks/DSA/Trees-and-Traversals/FilesAndFolders/Folder.cs
@@ -0,0 +1,27 @@
+﻿namespace FilesAndFolders
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    
+    public class Folder
+    {
+        public Folder(string name, string fullPath)
+        {
+            this.Name = name;
+            this.Directory = new DirectoryInfo(fullPath);
+            this.Files = new List<File>();
+            this.Folders = new List<Folder>();
+        }
+
+        public string Name { get; set; }
+        public List<File> Files { get; set; }
+        public List<Folder> Folders { get; set; }
+        public DirectoryInfo Directory { get; set; }
+
+        public long GetSize()
+        {
+            return this.Files.Sum(file => file.Size) + this.Folders.Sum(folder => folder.GetSize());
+        }
+    }
+}
