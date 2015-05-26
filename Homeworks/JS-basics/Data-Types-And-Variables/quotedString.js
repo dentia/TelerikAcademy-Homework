93cdcba155089aaada7d1e00262519de2857c9c2
dentia <dyct.ent@gmail.com>
2015-05-26 12:39:42 +0300

    js-basic deleted

--- Homeworks/JS-basics/Data-Types-And-Variables/quotedString.js
+++
@@ -1,9 +0,0 @@
-// Create a string variable with quoted text in it.
-// For example: 'How you doin'?', Joey said.
-
-var singleQuotes = '\'How you doin\'?\', Joey said.';
-var doubleQuotes = "'How you doin'?', Joey said.";
-
-var result = singleQuotes +'\n\r'+ doubleQuotes;
-
-console.log(result);
--- Homeworks/JS-basics/Loops/numbersFrom1ToN.js
+++
@@ -1,7 +0,0 @@
-// Write a script that prints all the numbers from 1 to N
-
-var n = 10;
-
-for(var number = 1; number <= n; number++){
-    console.log(number);
-}
