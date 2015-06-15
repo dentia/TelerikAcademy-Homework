5e833b4b3b778d445732bf524d27ca657fdd607e
dentia <dyct.ent@gmail.com>
2015-06-15 17:20:41 +0300

    [HW][HQC] Using-Variables-And-Constants
    Telerik Academy 2015-2016
    Using Variables And Constants

---
+++ Homeworks/HQC/Using-Variables-And-Constants/PrintArrayStatistics/DoubleArrayExtensionMethods.cs
@@ -0,0 +1,46 @@
+﻿namespace PrintArrayStatistics
+{
+    using System;
+    using System.Collections;
+    using System.Linq;
+    using System.Text;
+
+    public static class DoubleArrayExtensionMethods
+    {
+        public static void PrintStatistics<T>(this T[] numbers)
+            where T : IComparable
+        {
+            StringBuilder statistics = new StringBuilder();
+            statistics.AppendFormat("Min: {0}", numbers.GetMin()).AppendLine();
+            statistics.AppendFormat("Max: {0}", numbers.GetMax()).AppendLine();
+            statistics.AppendFormat("Avg: {0:F4}", numbers.GetAvg());
+
+            Console.WriteLine(statistics);
+        }
+
+        public static T GetMin<T>(this T[] numbers) 
+            where T : IComparable
+        {
+            return numbers.Min();
+        }
+
+        public static T GetMax<T>(this T[] numbers)
+            where T : IComparable
+        {
+            return numbers.Max();
+        }
+
+        public static T GetAvg<T>(this T[] numbers) 
+            where T : IComparable
+        {
+            dynamic sum = 0;
+
+            for (int ind = 0; ind < numbers.Length; ind++)
+            {
+                sum = sum + numbers[ind];
+            }
+
+            return (T)(sum / numbers.Length);
+        }
+    }
+}
---
+++ Homeworks/HQC/Using-Variables-And-Constants/SizeOperations/Size.cs
@@ -0,0 +1,44 @@
+﻿namespace SizeOperations
+{
+    using System;
+
+    public class Size
+    {
+        public Size(double width, double height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        /// <summary>
+        /// The method calculates the new size after of rotation by N degrees
+        /// </summary>
+        /// <param name="angleOfRotation">The degrees of rotation</param>
+        /// <returns>new object of type Size</returns>
+        public Size GetRotatedSize(double angleOfRotation)
+        {
+            double width = GetSinAndCosOfDimensions(angleOfRotation, this.Width, this.Height);
+            double height = GetSinAndCosOfDimensions(angleOfRotation, this.Height, this.Width);
+            Size sizeAfterRotation = new Size(width, height);
+
+            return sizeAfterRotation;
+        }
+
+        private double GetSinAndCosOfDimensions(double angle, double dimWithCos, double dimWithSin)
+        {
+            double dimensionWithCos = Math.Abs(Math.Cos(angle)) * dimWithCos;
+            double dimensionWithSin = Math.Abs(Math.Sin(angle)) * dimWithSin;
+
+            return dimensionWithCos + dimensionWithSin;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Width: {0:F3}, Height: {1:F3}", this.Width, this.Height);
+        }
+    }
+}
