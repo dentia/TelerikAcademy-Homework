e4145455d23f93db60ca6cdd8f2618f7473a5442
dentia <dyct.ent@gmail.com>
2015-06-21 01:22:22 +0300

    [HW][HQC] Unit-Testing
    Telerik Academy 2015-2016
    Unit Testing

---
+++ Homeworks/HQC/Unit-Testing/School/Validator.cs
@@ -0,0 +1,15 @@
+﻿namespace School
+{
+    public static class Validator
+    {
+        public static bool StringIsInvalid(string name)
+        {
+            return string.IsNullOrWhiteSpace(name);
+        }
+
+        public static bool IdIsInRange(int id, int min, int max)
+        {
+            return min <= id && id <= max;
+        }
+    }
+}
