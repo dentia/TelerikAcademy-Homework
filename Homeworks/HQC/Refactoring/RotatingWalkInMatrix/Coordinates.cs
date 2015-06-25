50f6cb37d43191b1a09cfa3c66d6f2d085dfa8e1
dentia <dyct.ent@gmail.com>
2015-06-25 15:31:34 +0300

    [HW][HQC] Refactoring
    Telerik Academy 2015-2016
    Refactoring

---
+++ Homeworks/HQC/Refactoring/RotatingWalkInMatrix/Coordinates.cs
@@ -0,0 +1,52 @@
+﻿namespace RotatingWalkInMatrix
+{
+    using System;
+
+    public class Coordinates
+    {
+        private int row;
+        private int col;
+
+        public Coordinates(int row, int col)
+        {
+            this.Row = row;
+            this.Col = col;
+        }
+
+        public int Row
+        {
+            get
+            {
+                return this.row;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Row cannot be negative.");
+                }
+
+                this.row = value;
+            }
+        }
+
+        public int Col
+        {
+            get
+            {
+                return this.col;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Col cannot be negative.");
+                }
+
+                this.col = value;
+            }
+        }
+    }
+}
---
+++ Homeworks/HQC/Refactoring/RotatingWalkInMatrix/RotatingWalkInMatrix.cs
@@ -0,0 +1,12 @@
+﻿namespace RotatingWalkInMatrix
+{
+    internal class RotatingWalkInMatrix
+    {
+        internal static void Main()
+        {
+            SquareMatrix matrix = new SquareMatrix(7);
+            matrix.RotatingWalkFill();
+            System.Console.WriteLine(matrix);
+        }
+    }
+}
