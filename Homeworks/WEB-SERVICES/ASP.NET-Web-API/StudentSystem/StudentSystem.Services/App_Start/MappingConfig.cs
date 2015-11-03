298d3a7223c19f10b1d95bc284a113c71b5c3ac2
Mihaela Encheva <dyct.ent@gmail.com>
2015-11-03 11:59:54 +0200

    mappings moved to separate class

---
+++ Homeworks/WEB-SERVICES/ASP.NET-Web-API/StudentSystem/StudentSystem.Services/App_Start/MappingConfig.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using StudentSystem.Models;
+using StudentSystem.Services.Models;
+
+namespace StudentSystem.Services
+{
+    public static class MappingConfig
+    {
+        public static void RegisterMappings()
+        {
+            Mapper.CreateMap<Student, StudentModel>()
+                .ForMember(
+                    dest => dest.Id,
+                    src => src.MapFrom(obj => obj.StudentIdentification));
+
+            Mapper.CreateMap<StudentModel, Student>()
+                .ForMember(
+                    dest => dest.StudentIdentification,
+                    src => src.MapFrom(obj => obj.Id));
+
+            Mapper.CreateMap<Course, CourseModel>()
+                .ForMember(
+                    dest => dest.CourseId,
+                    src => src.MapFrom(obj => obj.Id));
+
+            Mapper.CreateMap<CourseModel, Course>();
+
+            Mapper.CreateMap<Homework, HomeworkModel>();
+
+            Mapper.CreateMap<Test, TestModel>();
+        }
+    }
+}
--- Homeworks/WEB-SERVICES/ASP.NET-Web-API/StudentSystem/StudentSystem.Services/Startup.cs
+++ Homeworks/WEB-SERVICES/ASP.NET-Web-API/StudentSystem/StudentSystem.Services/Startup.cs
@@ -15,26 +15,7 @@
         {
             var config = new HttpConfiguration();
             WebApiConfig.Register(config);
-
-            Mapper.CreateMap<Student, StudentModel>()
-                .ForMember(
-                dest => dest.Id,
-                src => src.MapFrom(obj => obj.StudentIdentification));
-
-            Mapper.CreateMap<StudentModel, Student>()
-                .ForMember(
-                dest => dest.StudentIdentification,
-                src => src.MapFrom(obj => obj.Id));
-
-            Mapper.CreateMap<Course, CourseModel>()
-                .ForMember(
-                dest => dest.CourseId,
-                src => src.MapFrom(obj => obj.Id));
-            Mapper.CreateMap<CourseModel, Course>();
-
-            Mapper.CreateMap<Homework, HomeworkModel>();
-
-            Mapper.CreateMap<Test, TestModel>();
+            MappingConfig.RegisterMappings();
 
             app.UseNinjectMiddleware(() => NinjectConfig.CreateKernel.Value);
             app.UseNinjectWebApi(config);
